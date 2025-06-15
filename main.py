a5b37d31c24f93505900b2f554089ef55b8b813a
jakubpietrzko <81659075+jakubpietrzko@users.noreply.github.com>
2025-06-15 21:26:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from simulation import Simulation
-# Run the simulation
-if __name__ == "__main__":
-# we will play 100000 times, and in each simulation we will play 15 games how many times we will win, something like bernoulli with law of large numbers
+
+def bernoulli_simulation(trials, games):
+    """
+    Simulates a Bernoulli process for a given number of trials and games.
+    Returns the number of wins.
+    """
     counter = 0
-    for_for= 100000
-    games =15
-    for i in range(for_for):
+    for _ in range(trials):
         simulation = Simulation(games, display=False)
         if simulation.run():
             counter += 1
-    print(f"Winning probability whith switching when we played {games} games: {counter / for_for:.4f} ({counter} wins out of {for_for}simulations)")
-
-
+    return counter
+# Run the simulation
+if __name__ == "__main__":
+# we will play 100000 times, and in each simulation we will play 15 games how many times we will win, something like bernoulli with law of large numbers
+    bernoulli = bernoulli_simulation(100000, 15)
+    print(f"Number of wins in 100000 trials with 15 games each: {bernoulli}")
     # probability of winning in 1 games, with law of large numbers
     simulation = Simulation(100000, display=True)
     simulation.run()
